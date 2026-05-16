f350da1d3f6774c9e45319c6d0b31fcb0e53c572
AylethLimonxr <ximena.limonrdzz@gmail.com>
2026-05-16 13:36:53 -0600

    Se hicieron modificaciones a las clases de los estilos, se crearon mas archivos css y se actualizaron detalles del form

--- website_vet/static/scroll.js
+++ website_vet/static/scroll.js
@@ -1,16 +1,13 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry, index) => {
-        if (entry.isIntersecting) {
-            // Retraso escalonado para que aparezcan uno por uno
+const vista = new IntersectionObserver((entradas) => {
+    entradas.forEach((entrar, index) => {
+        if (entrar.isIntersecting) {
             setTimeout(() => {
-                entry.target.classList.add('visible');
+                entrar.target.classList.add('visible');
             }, index * 100);
         }
     });
-}, {
-    threshold: 0.1 // Aparece cuando el 10% del elemento es visible
-});
+})
 
-document.querySelectorAll('.animar').forEach(el => {
-    observer.observe(el);
+document.querySelectorAll('.animar').forEach(e => {
+    vista.observe(e);
 });
