4c73d1b8dccc484d71ea0ba887e849a1f1451c4b
AylethLimonxr <ximena.limonrdzz@gmail.com>
2026-05-15 21:39:40 -0600

    Se crearon los campos para agregar informacion en la base de datos

--- appVet/admin.py
+++ appVet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Doctor, Servicio, Producto, Sucursal, Empleado
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Servicio)
+admin.site.register(Producto)
+admin.site.register(Sucursal)
+admin.site.register(Empleado)
--- appVet/models.py
+++ appVet/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    nombre = models.CharField(max_length=50)
+    especialidad = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=10)
+    def __str__(self):
+        return self.nombre
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    precio = models.FloatField()
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=50)
+    precio = models.FloatField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
+    
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=50)
+    sucursal = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=10)
+    puesto = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombre
