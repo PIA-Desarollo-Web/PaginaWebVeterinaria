8846e9756f1b67ad7a5af3b6c5573b073372f993
AylethLimonxr <ximena.limonrdzz@gmail.com>
2026-05-15 19:09:11 -0600

    Se configuro las urls para que aparezcan las vistas en el sitio

--- appVet/urls.py
+++ appVet/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('' , views.inicio,name="inicio"), 
+    path('agendar' , views.agendarCita,name="inicio"), 
+    path('nosotros' , views.nosotros,name="inicio"), 
+    path('servicios' , views.servicios,name="inicio"), 
+    path('sucursales' , views.sucursales,name="inicio")
 ]
--- appVet/views.py
+++ appVet/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return render(request, "inicio.html")
+
+def agendarCita(request):
+    return render(request, "agendarCita.html")
+
+def nosotros(request):
+    return render(request, "nosotros.html")
+
+def servicios(request):
+    return render(request, "servicios.html")
+
+def sucursales(request):
+    return render(request, "sucursales.html")
